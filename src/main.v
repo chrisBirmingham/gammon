f9a1a99d4b1c6953bf00ca501110d717161048c3
Christopher Birmingham <chris.birmingham@hotmail.co.uk>
2024-01-29 22:31:36 +0000

    Fix default config location

--- src/main.v
+++ src/main.v
@@ -101,7 +101,7 @@ fn main() {
 		name: 'file'
 		abbrev: 'f'
 		description: 'Path to config file'
-		default_value: ['/etc/{gammon}.json']
+		default_value: ['/etc/${mod.name}.json']
 	})
 
 	app.add_flag(cli.Flag{
